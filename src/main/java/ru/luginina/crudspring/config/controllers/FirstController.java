076d2ce5fb44707150900a7695ec3a6626d43ca4
moerstany <moerstany@gmail.com>
2023-03-26 23:15:47 +0700

    Get запросы

--- src/main/java/ru/luginina/crudspring/config/controllers/FirstController.java
+++ src/main/java/ru/luginina/crudspring/config/controllers/FirstController.java
@@ -4,14 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/first")
 //контроллер
 public class FirstController {
     // методы
     @GetMapping("/hello")
-    public String HelloPage(){
-      return "first/hello";
+    public String HelloPage(HttpServletRequest request )/*внедрили объект для гет запросов в метод*/{
+      String name = request.getParameter("name");
+      String surname=request.getParameter("surname");
+      System.out.println("Hello" + name + surname);
+        return "first/hello";
     }
     @GetMapping("/goodbye")
     public String GoodbyePage(){
