dcd5373c5ff1365dd562cf38b9b1d01c46af1a73
moerstany <moerstany@gmail.com>
2023-04-01 20:14:01 +0700

    реализовали добавление записи

--- src/main/java/ru/luginina/crudspring/AutoDao/AutoDao.java
+++ src/main/java/ru/luginina/crudspring/AutoDao/AutoDao.java
@@ -3,37 +3,131 @@ package ru.luginina.crudspring.AutoDao;
 import org.springframework.stereotype.Component;
 import ru.luginina.crudspring.models.Auto;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 @Component
 public class AutoDao {
     private static int AvtoCount;
-    private List<Auto> autos;
 
-    {
-        autos = new ArrayList<>();
-        autos.add(new Auto(++AvtoCount, "Tractor", "Men", "v4837sd54", 5000.00));
-        autos.add(new Auto(++AvtoCount, "Tractor", "Chery", "v4667sd54", 3000.00));
+
+    private static final String URL = "snuffleupagus.db.elephantsql.com/mine";
+    private static final String USERNAME = "sjappxlr";
+    private static final String PASSWORD = "8pFqnAMCzYkIahI1OD7a3sHmKNY1Hyqs";
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
-    public List<Auto> index(){
+
+    public List<Auto> index() {
+        List<Auto> autos = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM auto";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while(resultSet.next()) {
+                Auto auto = new Auto();
+
+                auto.setId(resultSet.getInt("id"));
+                auto.setName(resultSet.getString("name"));
+                auto.setBrend(resultSet.getString("brend"));
+                auto.setNumber(resultSet.getString("number"));
+                auto.setPrice(resultSet.getDouble("price"));
+                autos.add(auto);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
         return autos;
     }
-    public Auto show(int id){
-        //найти авто с таким же id  в списке если его нет то вернуть ничто.
-        return autos.stream().filter(auto -> auto.getId() == id).findAny().orElse(null);
+    public Auto show(int id) {
+        Auto auto= null;
+
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("SELECT * FROM Auto WHERE id=?");
+
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            resultSet.next();
+
+            auto = new Auto();
+
+            auto.setId(resultSet.getInt("id"));
+            auto.setName(resultSet.getString("name"));
+            auto.setBrend(resultSet.getString("brend"));
+            auto.setNumber(resultSet.getString("number"));
+            auto.setPrice(resultSet.getDouble("price"));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return auto;
     }
-    public void save(Auto auto){
-        auto.setId(++AvtoCount);
-        autos.add(auto);
+
+    public void save(Auto auto) {
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("INSERT INTO Auto VALUES(1, ?, ?, ?, ? )");
+
+            preparedStatement.setString(1, auto.getName());
+            preparedStatement.setString(2, auto.getBrend());
+            preparedStatement.setString(3, auto.getNumber());
+            preparedStatement.setDouble(4, auto.getPrice());
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
-    public void update(int id, Auto updatedAuto){
-        Auto autoUpdated=show(id);
-        autoUpdated.setName(updatedAuto.getName());
-        autoUpdated.setBrend(updatedAuto.getBrend());
-        autoUpdated.setNumber(updatedAuto.getNumber());
-        autoUpdated.setPrice(updatedAuto.getPrice());
+
+    public void update(int id, Auto updatedAuto) {
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("UPDATE Auto SET name=?, brend=?, number=?, price=? WHERE id=?");
+
+            preparedStatement.setString(1, updatedAuto.getName());
+            preparedStatement.setString(2, updatedAuto.getBrend());
+            preparedStatement.setString(3, updatedAuto.getNumber());
+            preparedStatement.setDouble(4, updatedAuto.getPrice());
+            preparedStatement.setInt(5, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
-    public void delete(int id){
-        autos.removeIf(a->a.getId()==id);
+
+    public void delete(int id) {
+        PreparedStatement preparedStatement =
+                null;
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM Auto WHERE id=?");
+
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
     }
 }
+
